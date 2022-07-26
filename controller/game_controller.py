4b64a4e29c71a50f21d38b94be66be350a80617b
Alisa Malich <alisa.malich19@gmail.com>
2022-07-26 16:51:09 +0300

    Model.game is fixed

--- controller/game_controller.py
+++ controller/game_controller.py
@@ -17,10 +17,26 @@ class GameController:
                 #TODO display error message
                 row, col = self.view.get_move()
 
-            target_cell = (row, col)
-            score = []
-            for direction in self.DIRECTIONS:
-                curr_cell = target_cell
-                curr_cell = (curr_cell[0] + direction[0], curr_cell[1] + direction[1])
-                if self.model.is_opposite_cell(curr_cell[0], curr_cell[1])
+            # # save the cell program started from
+            # target_cell = (row, col)
+            # # 
+            # score = []
+            # for direction in self.DIRECTIONS:
+            #     curr_cell = target_cell
+            #     # make the first step in the direction
+            #     curr_cell = (curr_cell[0] + direction[0], curr_cell[1] + direction[1])
+            #     # continue making steps while the cell is on board and occupied by opponents disk
+            #     while self.model.is_on_board(curr_cell[0], curr_cell[1]) and self.model.is_opposite_cell(curr_cell[0], curr_cell[1]):
+            #         curr_cell = (curr_cell[0] + direction[0], curr_cell[1] + direction[1])
+            #     # after while loop check why it was interupted
+            #     # if there are no more steps on board -> returns to for loop and checking next direction
+            #     if not self.model.is_on_board(curr_cell[0], curr_cell[1]):
+            #         continue
+            #     # if current cell disk equals to current player disk 
+            #     if not self.model.is_opposite_cell(curr_cell[0], curr_cell[1]):
+
+                        
+
+                
+                
 
--- model/game.py
+++ model/game.py
@@ -2,6 +2,8 @@ from board import Board
 from player import Player
 
 class Game:
+    DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
     def __init__(self, board_size):
         self.board = Board.get_board(board_size)
         self.curr_player = Player.X
@@ -18,7 +20,13 @@ class Game:
             row (int)
             col (int)
         """
-        self.board.update_cell(row, col, self.curr_player)
+        steps = self.is_valid_move(row, col)
+        if len(steps) == 0:
+            return False
+        else:
+            for step in steps:
+                self.board.update_cell(step[0], step[1], self.curr_player)
+
 
     def is_empty_cell(self, row, col):
         """Check whether the cell value equals 0 
@@ -32,6 +40,9 @@ class Game:
         """
         return self.board.get_cell(row, col) == Board.EMPTY_CELL
 
+    def is_on_board(self, row, col):
+        return row <= self.board.size and col <= self.board.size 
+
     def is_opposite_cell(self, row, col):
         """Check whether the opposite disk on given cell
 
@@ -43,6 +54,33 @@ class Game:
             Bool
         """
         return self.board.get_cell(row, col) == self.curr_player
+
+    def is_valid_move(self, row, col):
+        # save the cell program started from
+        target_cell = (row, col)
+
+        for direction in self.DIRECTIONS:
+            curr_cell = target_cell
+            # make the first step in the direction
+            curr_cell = (curr_cell[0] + direction[0], curr_cell[1] + direction[1])
+            # continue making steps while the cell is on board and occupied by opponents disk
+            while self.is_on_board(curr_cell[0], curr_cell[1]) and self.is_opposite_cell(curr_cell[0], curr_cell[1]):
+                curr_cell = (curr_cell[0] + direction[0], curr_cell[1] + direction[1])
+            # after while loop check why it was interupted
+            # if there are no more steps on board -> returns to for loop and checking next direction
+            if not self.is_on_board(curr_cell[0], curr_cell[1]):
+                continue
+            # if current cell disk equals to current player disk 
+            if not self.is_opposite_cell(curr_cell[0], curr_cell[1]):
+                # create a list to store all steps
+                steps = []
+                while curr_cell[0] == target_cell[0] and curr_cell[1] == target_cell[1]:
+                    curr_cell[0] -= direction[0]
+                    curr_cell[1] -= direction[1]
+                    steps.append((curr_cell[0], curr_cell[1]))
+            return steps
+
+
 
     # def checking_directions(self, row, col):
     #     target_cell = (row, col)
